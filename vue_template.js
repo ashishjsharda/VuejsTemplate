f1b703a9ee13ac6683edd0d1a5a104c3453806f8
Ashish S <ashishjsharda@gmail.com>
2019-11-23 20:22:42 -0500

    Create vue_template.js

---
+++ vue_template.js
@@ -0,0 +1,10 @@
+var myObject = new Vue({
+  el: '#app',
+  data: {
+  message: 'Hello Vue!',
+  firstname:'Ashish',
+  lastname:'Sharda',
+  htmlContent:'<h1> This is from htmlContent </h1>'
+  }
+})
+
